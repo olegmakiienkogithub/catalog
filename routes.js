17ec64945ae6fc260d76818465b2f600ae42d6a0
Oleg Maki <olegmakiienkogithub@gmail.com>
2016-07-17 16:28:49 +0200

    Refactor routes

--- routes.js
+++ routes.js
@@ -4,62 +4,46 @@
     Routing table
  */
 const advertService = require('./service/advert');
+/*
+    Handle results and wrap into json
+ */
+function getHandler(res) {
+    return function(e, data) {
+        if(e) { return res.status(400).json({ error: e }); }
+        res.json({ data: data });
+    };
+};
 
 exports.setup = function(app, db) {
     /*
         Create
      */
     app.post('/advert', function(req, res) {
-        advertService.create(db, req.body, (e, data) => {
-            if(e) {
-                return res.status(400).json({ error: e });
-            }
-            res.json({ data: data });
-        });
+        advertService.create(db, req.body, getHandler(res));
     });
     /*
         Update
      */
     app.post('/advert/:id', function(req, res) {
-        advertService.update(db, req.params.id, req.body, (e, data) => {
-            if(e) {
-                return res.status(400).json({ error: e });
-            }
-            res.json({ data: data });
-        });
+        advertService.update(db, req.params.id, req.body, getHandler(res));
     });
     /*
         Get all
      */
     app.get('/advert', function(req, res) {
-        advertService.getAll(db, req.query, (e, data) => {
-            if(e) {
-                return res.status(400).json({ error: e });
-            }
-            res.json({ data: data });
-        });
+        advertService.getAll(db, req.query, getHandler(res));
     });
     /*
         Get by ID
      */
     app.get('/advert/:id', function(req, res) {
-        advertService.getById(db, { id: req.params.id }, (e, data) => {
-            if(e) {
-                return res.status(400).json({ error: e });
-            }
-            res.json({ data: data });
-        });
+        advertService.getById(db, { id: req.params.id }, getHandler(res));
     });
 
     /*
         Delete by ID
      */
     app.delete('/advert/:id', function(req, res) {
-        advertService.deleteById(db, { id: req.params.id }, (e, data) => {
-            if(e) {
-                return res.status(400).json({ error: e });
-            }
-            res.json({ data: data });
-        });
+        advertService.deleteById(db, { id: req.params.id }, getHandler(res));
     });
 };
